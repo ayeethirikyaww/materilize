0300e00e2a020700654839be4e8e216d8ddf0204
Aye Thiri Kyaw <39292408+ayeethirikyaww@users.noreply.github.com>
2021-01-15 20:05:17 +0630

    Update main.py
    write origin main.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from flask import Flask, render_template, url_for
-import sys
-import logging
+
 app = Flask(__name__)
 
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
-
-@app.route("/home")
+@app.route("/")
 
 def home():
     return render_template('navbar.html')
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
