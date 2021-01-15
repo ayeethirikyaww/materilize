bda579638f02e45f12bbae5e215ff7071c462417
Aye Thiri Kyaw <39292408+ayeethirikyaww@users.noreply.github.com>
2021-01-15 21:09:25 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, url_for
-
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 
 def home():
     return render_template('navbar.html')
 
-if __name__ == "__main__":
-    app.run(debug=False)
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
